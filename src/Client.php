24ab3946291df8720ad1048252d29be0fef1824d
J-P Schreiber <jpschreiber@sdv.fr>
2020-04-21 13:33:29 +0200

    Update clarifai authentifcation

--- src/Client.php
+++ src/Client.php
@@ -18,18 +18,6 @@ class Client
     protected $clientId;
 
     /**
-     * The Clarifai client secret.
-     * @var string
-     */
-    protected $clientSecret;
-
-    /**
-     * The Clarifai access token.
-     * @var string
-     */
-    protected $accessToken;
-
-    /**
      * The Clarifai API base URL.
      * @var string
      */
@@ -40,20 +28,11 @@ class Client
      *
      * @param HttpClient $client
      * @param string $clientId
-     * @param string $clientSecret
      */
-    public function __construct($client, $clientId, $clientSecret)
+    public function __construct($client, $clientId)
     {
         $this->client = $client;
         $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
-    }
-
-    public function withAccessToken($accessToken)
-    {
-        $this->accessToken = $accessToken;
-
-        return $this;
     }
 
     /**
@@ -75,27 +54,9 @@ class Client
             'headers' => [
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer ' . $this->accessToken,
+                'Authorization' => 'Key ' . $this->clientId,
             ],
             'json' => $body,
-        ]);
-
-        return json_decode($response->getBody(), true);
-    }
-
-    /**
-     * Retrieve a new access token.
-     *
-     * @return array
-     */
-    public function accessToken()
-    {
-        $response = $this->client->request('POST', $this->baseUrl . '/token', [
-            'headers' => [
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-            ],
-            'auth' => [$this->clientId, $this->clientSecret],
         ]);
 
         return json_decode($response->getBody(), true);
